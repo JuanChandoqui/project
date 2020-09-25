d739e47b3d0af4f866ebdc3a24e92e8b4733e0a2
JuanChandoqui <JuanChandoqui@users.noreply.github.com>
2020-09-24 19:15:07 -0500

    httpclient add on services

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {ServiceV2Service} from '../service-v2.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
+
+//CLASE PRINCIPAL DE COMPONENTE DE LA LOGICA DEL NEGOCIO
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  products = [];
+
+  constructor(private serviceV2Service: ServiceV2Service) { }
 
   ngOnInit(): void {
+
+    this.serviceV2Service.getProduct("products/").subscribe((data : any []) =>{
+
+      console.log(data);
+      this.products = data;
+    });
   }
 
 }
--- src/app/service-v2.service.ts
+++ src/app/service-v2.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ServiceV2Service {
 
-  constructor() { }
+  private RES_API_SERVER = "http://localhost:3000/";
+
+  constructor(private httpclient : HttpClient) { }
+
+  public getProduct(nameEndPoint: String){
+    return this.httpclient.get(this.RES_API_SERVER + nameEndPoint);
+  }
 }
+
